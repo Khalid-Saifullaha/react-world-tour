d57d15bb3e110871f45abadc619152e4a99c4504
Your Name <khalidsaifullaha3@gmail.com>
2024-10-19 23:35:19 +0600

    rest country show and visited country add

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+// import Countries from './components/Countries/Countries'
+import Countries from './components/Contries/Countries'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+      <h1>React World Tour</h1>
+     <Countries></Countries>
+ 
+    </>
+  )
+}
+
+export default App
